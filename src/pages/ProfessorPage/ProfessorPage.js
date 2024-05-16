4043cced4b67611ad48bd7dc9de26af5649e6ea4
lotfiengineer <mohammad.lotfi7913@gmail.com>
2024-05-16 21:11:27 +0330

    implemented api call

--- src/pages/ProfessorPage/ProfessorPage.js
+++ src/pages/ProfessorPage/ProfessorPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ProfessorPage = () => {
-  return (
-    <div>ProfessorPage</div>
-  )
-}
+  const [count, setCount] = useState("");
 
-export default ProfessorPage
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/") // api for the get request
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return <div>ProfessorPage</div>;
+};
+
+export default ProfessorPage;
