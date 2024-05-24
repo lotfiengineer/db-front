dd1eaac15b1be429bfd7f26d4dd2a563bf9c7d7d
lotfiengineer <mohammad.lotfi7913@gmail.com>
2024-05-24 12:44:57 +0330

    implemented courses get feature

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,5 +2,6 @@ var baseUrl = "http://localhost:3003";
 
 var professorsUrl = baseUrl + "/professors";
 var studentsUrl = baseUrl + "/students";
+var coursesUrl = baseUrl + "/courses";
 
-export { professorsUrl, studentsUrl };
+export { professorsUrl, studentsUrl, coursesUrl };
--- src/pages/CoursesPage/CoursesPage.js
+++ src/pages/CoursesPage/CoursesPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { coursesUrl } from "../../constants/urls";
 
 const CoursesPage = () => {
-  return (
-    <div>CoursesPage</div>
-  )
-}
+  const [coursesData, setCoursesData] = useState([]);
 
-export default CoursesPage
+  useEffect(() => {
+    fetch(coursesUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setCoursesData(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Course ID</th>
+            <th>Course Title</th>
+            <th>Department Name</th>
+            <th>Credits</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {coursesData.map((item) => (
+            <tr key={item.ID}>
+              <th>{item.course_id}</th>
+              <th>{item.title}</th>
+              <th>{item.dept_name}</th>
+              <th>{item.credits}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoursesPage;
